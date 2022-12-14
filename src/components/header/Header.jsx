0444f5ad1b0fc5c977114cd65a4d1b3a2d563c9d
vinay chandran <vinay.sarvodaya1991@gmail.com>
2022-12-14 21:26:40 +0530

    add  contents in header module

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './header.css'
+import Cta from './Cta'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="container header_container">
+      <h5>Hello I'm</h5>
+      <h1>Vinay Chandran</h1>
+      <h5 className='text-light'>Front-End Developer</h5>
+      <Cta />
+
+      <div className='me'>
+        <img src='' alt="me" />
+      </div>
+      <a href="#contact" className='scroll_down'>Scroll Down</a>
+    </div>
   )
 }
 
